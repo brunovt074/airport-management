3c283df75dbc9baef4da1c59e643551b66065bda
brunovt074 <brunovt.074@gmail.com>
2023-09-21 12:15:25 -0300

    creado AeropuertoDAO

---
+++ src/main/java/com/tsti/dao/AeropuertoDAO.java
@@ -0,0 +1,27 @@
+package com.tsti.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tsti.entidades.Ciudad;
+/**
+ * @author Bruno
+ *
+ */
+
+@Repository
+public interface AeropuertoDAO extends JpaRepository<Ciudad, String>{
+
+	public boolean existsByIcao(String icao);
+	public boolean existsByName(String name);
+	public boolean existsByCity(String city);
+	public boolean existsByState(String state);
+	public boolean existsByCountry(String country);
+	
+	public boolean findByIcao(String icao);
+	public boolean findByName(String name);
+	public boolean findByCity(String city);
+	public boolean findByState(String state);
+	public boolean findByCountry(String country);
+	
+}
