539ba0539c44ff78b95e94f9a4a0d9cc77617e52
brunovt074 <brunovt.074@gmail.com>
2023-09-28 13:52:32 -0300

    refactor main class

--- src/main/java/com/tsti/Daos2023Application.java
+++ src/main/java/com/tsti/Daos2023Application.java
@@ -3,10 +3,7 @@ package com.tsti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
@@ -22,18 +19,6 @@ public class Daos2023Application implements WebMvcConfigurer, AppShellConfigurat
 	
 	private static final long serialVersionUID = -3121037374624477448L;
 
-	/*
-	 * private final LocaleChangeInterceptor localeChangeInterceptor;
-	 * 
-	 * 
-	 * public Daos2023Application(LocaleChangeInterceptor localeChangeInterceptor) {
-	 * super(); this.localeChangeInterceptor = localeChangeInterceptor; }
-	 * 
-	 * @Override public void addInterceptors(InterceptorRegistry
-	 * interceptorRegistry) {
-	 * interceptorRegistry.addInterceptor(localeChangeInterceptor); }
-	 */	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Daos2023Application.class, args);	
 		
